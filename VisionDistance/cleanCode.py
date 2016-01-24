653b51e604d628589bc4e22922038a741fce2a7c
Michael Laposata <MichaelLaposata@Michaels-MacBook-Pro.local>
2016-01-24 14:50:35 -0500

    Merge branch 'master' of https://github.com/FRC125/VISION16
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- VisionDistance/cleanCode.py
+++ VisionDistance/cleanCode.py
@@ -63,6 +63,35 @@ def getPixelWidth(image):
 def getAngle(dist, x):
     return math.atan(float(x)/float(dist))
 
+#NumpyArray -> NumpyArray
+def order_points(pts):
+    print(pts)
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    print(s.flatten())
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    
+    pts = np.delete(pts, [np.argmin(s), np.argmax(s)], 0)
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+    
 #Mask bit color? Do you want to remove noisy
 im = cv2.imread("green4.JPG")
 im = resize(im,1000,700)
