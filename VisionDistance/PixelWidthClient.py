2c5a173fabf918ef2ef23aa3610787288e35eb8a
Bryan Menjivar <Bmenjivar89@gmail.com>
2016-01-25 20:39:48 -0500

    Spaced out code

--- VisionDistance/PixelWidthClient.py
+++ VisionDistance/PixelWidthClient.py
@@ -6,7 +6,6 @@ KNOWN_DISTANCE = 24
 
 #Store the actual Known width and height of the object as constants. 
 IMAGES = ['2ft.jpg', '4ft.jpg', '6ft.jpg', 'tilt.jpg']
-
 #Inialize Array to store the paths to the pictures (ex: IMAGES = ['pathToFirstPic.jpg','pathToSecondPic.jpg', 'soOnAnSoForth.jpg'])
 #No need to specify path if pictures are in the same directory as the code 
 class Runner(calculateDistance):
@@ -21,7 +20,6 @@ class Runner(calculateDistance):
                 return (KNOWN_WIDTH * FOCAL_LENGTH)/self.w
 
 
-    
 initialIm = Runner(IMAGES[0])
 FOCAL_LENGTH = initialIm.calculateFocalLength()
 finalIm = Runner(IMAGES[1])
@@ -31,13 +29,14 @@ w,h = initialIm.computeHeightWidth()
 print "finalpwidth: " + str(finalIm.w)
 print "initialpwidth" + str(initialIm.w)
 print "focalLength " + str(FOCAL_LENGTH)
-
 print str()
 
 
 Distance = finalIm.calculateDistance()
 
 print "Distance: " + str(Distance)
-##           cv2.imshow("hello", initialIm.im)
-##           cv2.waitKey(0)
-##           cv2.destroyAllWindows()
+
+
+##  cv2.imshow("hello", initialIm.im)
+##  cv2.waitKey(0)
+##  cv2.destroyAllWindows()
