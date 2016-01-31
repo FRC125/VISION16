6bc2a4e6842028756090884ecc5752828135009b
Badr Bouhou <camilophone@live.com>
2016-01-31 00:31:14 -0500

    added basic vars

--- vision16.py
+++ vision16.py
@@ -2,3 +2,10 @@ import sys
 import time
 import logging
 from networktables import NetworkTable
+
+logging.basicConfig(level=logging.DEBUG)
+ip = 127.0.0.1
+
+NetworkTable.setIPAddress(ip)
+NetworkTable.setClientMode()
+NetworkTable.initialize()
