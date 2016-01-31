3f0e7aa7a2aec4da5596306239767e8bd1b048ac
Badr Bouhou <camilophone@live.com>
2016-01-31 00:17:46 -0500

    fixed random indents

--- VisionDistance/PixelWidthClient.py
+++ VisionDistance/PixelWidthClient.py
@@ -4,13 +4,12 @@ from PixelWidth import *
 KNOWN_WIDTH = 20
 KNOWN_DISTANCE = 24
 
-#Store the actual Known width and height of the object as constants. 
+#Store the actual Known width and height of the object as constants.
 IMAGES = ['2ft.jpg', '4ft.jpg', '6ft.jpg', 'tilt.jpg']
 
 #Inialize Array to store the paths to the pictures (ex: IMAGES = ['pathToFirstPic.jpg','pathToSecondPic.jpg', 'soOnAnSoForth.jpg'])
-#No need to specify path if pictures are in the same directory as the code 
+#No need to specify path if pictures are in the same directory as the code
 class Runner(calculateDistance):
-
 
     def calculateFocalLength(self):
                 #FOCAL LENGTH = (w x KNOWN_DISTANCE) / KNOWN_WIDTH
@@ -32,12 +31,6 @@ print "initialpwidth" + str(initialIm.w)
 print "focalLength " + str(FOCAL_LENGTH)
 print  str()
 
-
 Distance = finalIm.calculateDistance()
 
 print "Distance: " + str(Distance)
-
-
-##  cv2.imshow("hello", initialIm.im)
-##  cv2.waitKey(0)
-##  cv2.destroyAllWindows()
