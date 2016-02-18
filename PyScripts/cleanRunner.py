0b4d1179e338651c65097690f4514008d2e89bd0
basedTaha <tazzaoui617@gmail.com>
2016-02-17 19:32:04 -0500

    Troubleshoot comms with pynetworktables

--- PyScripts/cleanRunner.py
+++ PyScripts/cleanRunner.py
@@ -7,7 +7,6 @@ cameraWidth = 960
 #Read and resize the two images (edit param to include respective path to image)
 img = resize(cv2.imread("Pics/DocExport1/10_20.png"),cameraHeight,cameraWidth)
 
-
 #Thresh using the Low and High limits and remove noise
 initialMask = bit_color(img, LOW_GREEN, HIGH_GREEN)
 
--- PyScripts/v16Client.py
+++ PyScripts/v16Client.py
@@ -1,5 +1,6 @@
 import cv2
 from v16Parent import *
+import Server
 
 camera_port = 0
 
@@ -8,40 +9,53 @@ cap = cv2.VideoCapture(camera_port)
 #If issues stem, change Height and Width Settings (Preserve Aspect Ratio)
 
 cap.set(3, 1280) #Set the width important because the default will timeout
-cap.set(4, 720) #Set the height (ignore the errors printed to the console)
+cap.set(4, 960) #Set the height (ignore the errors printed to the console)
 
 
 # Capture a single image from the camera and return it
 def get_image():
  r, frame = cap.read()
  return frame
- 
-img = get_image()
 
-#Thresh using the Low and High constant limits to remove noise
-initialMask = bit_color(img, LOW_GREEN, HIGH_GREEN)
+while True:
 
-#Draw the corners onto the image
-corners = drawCorners(initialMask,img)
+    try:
+        img = get_image()
 
-#Save the image
-cv2.imwrite("test.jpg", img)
+        #Thresh using the Low and High constant limits to remove noise
+        initialMask = bit_color(img, LOW_GREEN, HIGH_GREEN)
 
-OrderInitCorners = order_points(np.array(corners))
+        #Draw the corners onto the image
+        corners = drawCorners(initialMask,img)
 
-LeftHeight, RightHeight = getHeightLeftRight
-avgHeight = (LeftHeight+RightHight)/2
-slope = getSlope(OrderInitCorners)
-width = getWidth(OrderInitCorners)
-aspectRatio = getAspectRatio(avgHeight, width)
-theta = getAngle(aspectRatio)
-offsetAngle = getOffsetAngle(OrderInitCorners)
-distance = getDistance(OrderInitCorners)
-offsetDistance = getOffsetDistance(theta,distance) 
+        #Save the image
+        cv2.imwrite("test.jpg", img)
 
-print("Distance From Target",distance)
-print("Angle Between Robot and Target",theta)
-print("Offset Angle", offsetAngle)
-print("Offset Horizontal Distance", offserDistance)
+        OrderInitCorners = order_points(np.array(corners))
+
+        LeftHeight, RightHeight = getHeightLeftRight(OrderInitCorners)
+        avgHeight = (LeftHeight+RightHeight)/2
+        slope = getSlope(OrderInitCorners)
+        width = getWidth(OrderInitCorners)
+        aspectRatio = getAspectRatio(avgHeight, width)
+        theta = getAngle(aspectRatio)
+        offsetAngle = getOffsetAngle(OrderInitCorners)
+        distance = getDistance(OrderInitCorners)
+        offsetDistance = getOffsetDistance(theta,distance)
+
+        
+
+        print("Distance From Target",distance)
+        Server.setDistance(distance)
+        print("Angle Between Robot and Target",theta)
+        print("Offset Angle", offsetAngle)
+        Server.setOffsetAngle(offsetAngle)
+        print("Offset Horizontal Distance", offsetDistance)
+        
+    except ImageNotDetectedException:
+        Server.canSee()
+
+        
+        
 
 del(cap)
